0932d3c3b942031e4a221c7ef7fee535661c2ee4
Coy Sanders <coymeetsworld@gmail.com>
2016-12-12 02:56:32 -0800

    Add navigation

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,8 @@ module.exports = {
 		alias: { /* Webpack aliases */
 			Main: 'app/components/Main.jsx',
 			Navigation: 'app/components/Navigation.jsx',
+			Timer: 'app/components/Timer.jsx',
+			Countdown: 'app/components/Countdown.jsx',
 			applicationStyles: 'app/styles/app.scss'
 		},
 		extensions: ['', '.js', '.jsx']
