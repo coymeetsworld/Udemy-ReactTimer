b73d98f5a4553fc8ceed0a1614ee3c8505f41555
Coy Sanders <coymeetsworld@gmail.com>
2016-12-14 15:36:43 -0800

    Finalize tests and add page title to countdown

--- app/tests/components/Countdown.test.jsx
+++ app/tests/components/Countdown.test.jsx
@@ -33,10 +33,32 @@ describe('Countdown', () => {
 
 			setTimeout(() => {
 				expect(countdown.state.count).toBe(0);
-				//expect(countdown.state.countdownStatus).toBe('stopped');
 				done(); /* Used when you expect an asynchronous test. */
 			}, 5001);
+		});
 		
+		it('should pause countdown on paused status', () => {
+			var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+			countdown.handleSetCountdown(3);
+			countdown.handleStatusChange('paused');
+			
+			setTimeout(() => {
+				expect(countdown.state.count).toBe(3);
+				expect(countdown.state.countdownStatus).toBe('paused');
+				done(); /* Used when you expect an asynchronous test. */
+			}, 1001);
+		});
+
+		it('should stop countdown on stopped status and reset count to 0', () => {
+			var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+			countdown.handleSetCountdown(3);
+			countdown.handleStatusChange('stopped');
+			
+			setTimeout(() => {
+				expect(countdown.state.count).toBe(0);
+				expect(countdown.state.countdownStatus).toBe('stopped');
+				done(); /* Used when you expect an asynchronous test. */
+			}, 1001);
 		});
 	
 /*
